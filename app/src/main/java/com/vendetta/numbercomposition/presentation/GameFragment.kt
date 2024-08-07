3160529d4117756122a596a61c570a4c277bac9b
sasha <berezhalex90@gmail.com>
2024-08-07 12:32:55 +0300

    add viewBinding on all fragments

--- app/src/main/java/com/vendetta/numbercomposition/presentation/GameFragment.kt
+++ app/src/main/java/com/vendetta/numbercomposition/presentation/GameFragment.kt
@@ -5,15 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.vendetta.numbercomposition.R
+import com.vendetta.numbercomposition.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
+
+    private var _binding:FragmentGameBinding? = null
+    private val binding:FragmentGameBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_game, container, false)
+    ): View {
+        _binding = FragmentGameBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/vendetta/numbercomposition/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/vendetta/numbercomposition/presentation/WelcomeFragment.kt
@@ -5,15 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.vendetta.numbercomposition.R
+import com.vendetta.numbercomposition.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding: FragmentWelcomeBinding
+        get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonUnderstand.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
